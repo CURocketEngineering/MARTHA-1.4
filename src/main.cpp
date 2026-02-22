668c02753c5470742d0f387332e88d4b20a6d3da
Elan456 <telan4892@gmail.com>
2026-02-22 17:48:49 -0500

    change apogee predictor back to analytic_update

--- src/main.cpp
+++ src/main.cpp
@@ -354,7 +354,7 @@ void loop() {
 
   // If post-launch, then start saving estimated apogee data
   if (stateMachine.getState() >= STATE_ASCENT) {
-    apogeePredictor.poly_update();
+    apogeePredictor.analytic_update();
     groundLevelEstimator.launchDetected();
     apogeeEstData.addData(DataPoint(current_time, apogeePredictor.getPredictedApogeeAltitude_m()));
   }
